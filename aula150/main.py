a8fdc05bcaf616d20509da90a31a82b1a0ee6962
Kelvin Alisson Cantarino <kallissin@users.noreply.github.com>
2021-01-24 16:10:20 -0300

    GRUD concluido

--- aula150/main.py
+++ aula150/main.py
@@ -1,20 +1,67 @@
 import pymysql.cursors
+from contextlib import contextmanager
 
-conexao = pymysql.connect(
-    host='127.0.0.1',
-    user='root',
-    password='',
-    db='clientes',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
+# CRUD = CREATE, READ, UPDATE, DELETE
+
+@contextmanager
+def conecta():
+    conexao = pymysql.connect(
+        host='127.0.0.1',
+        user='root',
+        password='',
+        db='clientes',
+        charset='utf8mb4',
+        cursorclass=pymysql.cursors.DictCursor
 )
+    try:
+        yield conexao
+    finally:
+        conexao.close()
 
-with conexao.cursor() as cursor:
-    cursor.execute('SELECT * FROM  clientes')
-    resultado = cursor.fetchall()
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES' \
+#               '(%s, %s, %s, %s)'
+#         cursor.execute(sql, ('Jack', 'Monroe', 112, 220))
+#         conexao.commit()
 
-    print(resultado)
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'INSERT INTO clientes (nome, sobrenome, idade, peso) VALUES' \
+#               '(%s, %s, %s, %s)'
+#         dados = [
+#             ('MURIEL', 'FIGUEIREDO', 19, 55),
+#             ('ROSE', 'FIGUEIREDO', 19, 55),
+#             ('JOSE', 'FIGUEIREDO', 19, 55),
+#         ]
+#
+#         cursor.executemany(sql, dados)
+#
+#         conexao.commit()
 
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id = %s'
+#         cursor.execute(sql, (6,))
+#         conexao.commit()
 
+# with conecta() as conexao:
+#     with conexao.cursor() as cursor:
+#         sql = 'DELETE FROM clientes WHERE id BETWEEN %s AND %s'
+#         cursor.execute(sql, (10, 12))
+#         conexao.commit()
 
-conexao.close()
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        sql = 'UPDATE clientes SET nome=%s WHERE id=%s'
+        cursor.execute(sql, ('JOANA', 5))
+        conexao.commit()
+
+with conecta() as conexao:
+    with conexao.cursor() as cursor:
+        cursor.execute('SELECT * FROM  clientes ORDER BY id ASC LIMIT 100')
+        resultado = cursor.fetchall()
+
+        for linha in resultado:
+            print(linha)
+
